33c701aa85bb6e0781520889266edcf7a6f7d6ad
孙翼 <38261135@qq.com>
2024-06-03 09:48:25 -0600

    update

--- train.py
+++ train.py
@@ -10,39 +10,43 @@ from tensorboard_logger import TensorBoardLogger
 
 
 class Train:
-    def __init__(self, env_config, network_config, training_config):
+    def __init__(self, env_config, network_config, training_config, data_file=None):
         self.data_preprocessor = DataPreprocessor(
             env_config["max_entities"], env_config["max_tasks"], env_config["entity_dim"], env_config["task_dim"])
-        self.dataset = SampleGenerator(
-            training_config["num_samples"], self.data_preprocessor)
+
+        if data_file:
+            self.dataset = SampleGenerator(
+                training_config["num_samples"], self.data_preprocessor, data_file)
+        else:
+            self.dataset = SampleGenerator(
+                training_config["num_samples"], self.data_preprocessor)
+            self.dataset.save_data("train_data.h5")
+
         self.dataloader = DataLoader(
             self.dataset, batch_size=training_config["batch_size"], shuffle=True, collate_fn=self.collate_fn)
 
         self.val_dataset = SampleGenerator(
-            training_config["num_samples"] // 10, self.data_preprocessor)  # 假设验证集为训练集的10%
+            training_config["num_samples"] // 10, self.data_preprocessor, data_file)
         self.val_dataloader = DataLoader(
             self.val_dataset, batch_size=training_config["batch_size"], shuffle=False, collate_fn=self.collate_fn)
 
         self.device = torch.device(
             "cuda" if torch.cuda.is_available() else "cpu")
 
-        self.model = DecisionNetworkMultiHead(env_config["entity_dim"], env_config["task_dim"], network_config["transfer_dim"], network_config["entity_num_heads"],
-                                              network_config["task_num_heads"], network_config["hidden_dim"], network_config["num_layers"], network_config["mlp_hidden_dim"], env_config["max_entities"], network_config["output_dim"])
+        self.model = DecisionNetworkMultiHead(env_config["entity_dim"], env_config["task_dim"], network_config["transfer_dim"],
+                                              network_config["entity_num_heads"], network_config["task_num_heads"], network_config["hidden_dim"], network_config["num_layers"], network_config["mlp_hidden_dim"], env_config["max_entities"], network_config["output_dim"] + 1)  # 增加一个任务编号
         self.model.to(self.device)
-        self.model.apply(init_weights)
 
-        self.criterion = nn.CrossEntropyLoss(ignore_index=-1)  # 忽略 -1 标签
+        self.criterion = nn.CrossEntropyLoss(ignore_index=-1)
         self.optimizer = optim.Adam(
             self.model.parameters(), lr=training_config["lr"])
         self.scheduler = ReduceLROnPlateau(
             self.optimizer, mode='min', factor=0.1, patience=5)
 
         self.num_epochs = training_config["num_epochs"]
-        self.patience = training_config.get(
-            "patience", 100)  # 默认早停耐心值为10个epoch
+        self.patience = training_config.get("patience", 10)
         self.logger = TensorBoardLogger()
 
-        # Log the model graph (structure) to TensorBoard outside of training loop
         dummy_entities = torch.zeros(
             (training_config["batch_size"], env_config["max_entities"], env_config["entity_dim"])).to(self.device)
         dummy_tasks = torch.zeros(
@@ -52,8 +56,8 @@ class Train:
         dummy_task_mask = torch.ones(
             (training_config["batch_size"], env_config["max_tasks"])).to(self.device)
 
-        # self.logger.log_graph(
-        #     self.model, (dummy_entities, dummy_tasks, dummy_entity_mask, dummy_task_mask))
+        self.logger.log_graph(
+            self.model, (dummy_entities, dummy_tasks, dummy_entity_mask, dummy_task_mask))
 
     def collate_fn(self, batch):
         entities, tasks, entity_mask, task_mask, task_assignments = zip(*batch)
@@ -61,10 +65,6 @@ class Train:
         tasks = torch.stack(tasks)
         entity_mask = torch.stack(entity_mask)
         task_mask = torch.stack(task_mask)
-
-        # 保留 -1 表示无效数据
-        task_assignments = [torch.tensor(
-            ta, dtype=torch.long) for ta in task_assignments]
         task_assignments = torch.stack(task_assignments)
         return entities, tasks, entity_mask, task_mask, task_assignments
 
@@ -76,12 +76,13 @@ class Train:
                 entities, tasks, entity_mask, task_mask, targets = entities.to(self.device), tasks.to(
                     self.device), entity_mask.to(self.device), task_mask.to(self.device), targets.to(self.device)
 
-                outputs = self.model(
-                    entities, tasks, entity_mask, task_mask)
+                outputs = self.model(entities, tasks, entity_mask, task_mask)
+                if outputs is None:
+                    continue
 
                 loss = 0
                 for i, output in enumerate(outputs):
-                    loss += self.criterion(output, targets[i])
+                    loss += self.criterion(output, targets[:, i])
 
                 total_val_loss += loss.item()
 
@@ -114,32 +115,25 @@ class Train:
 
                 self.optimizer.zero_grad()
 
-                # 检查并处理 entity_mask 和 task_mask 中全为 1 的情况
-                if torch.any(entity_mask.sum(dim=1) == entity_mask.size(1)):
-                    entity_mask[entity_mask.sum(
-                        dim=1) == entity_mask.size(1)] = 0
-                if torch.any(task_mask.sum(dim=1) == task_mask.size(1)):
-                    task_mask[task_mask.sum(dim=1) == task_mask.size(1)] = 0
-
                 outputs = self.model(entities, tasks, entity_mask, task_mask)
+                if outputs is None:
+                    continue
 
-                # 确保outputs和task_assignments的维度匹配
+                outputs = torch.stack(outputs, dim=1)
                 assert outputs.shape[:-
                                      1] == task_assignments.shape, "输出和任务分配的维度不匹配"
 
-                # 计算每个平台对应任务的损失
+                valid_mask = task_assignments != -1
+                valid_task_assignments = task_assignments[valid_mask]
+                valid_outputs = outputs[valid_mask]
+
                 loss = 0
-                for i in range(outputs.shape[1]):
-                    valid_mask = (task_assignments[:, i] != -1)
-                    valid_outputs = outputs[valid_mask, i, :]
-                    valid_task_assignments = task_assignments[valid_mask, i]
-                    if valid_outputs.size(0) > 0:
-                        loss += self.criterion(valid_outputs,
-                                               valid_task_assignments)
+                for i in range(valid_outputs.shape[1]):
+                    loss += self.criterion(valid_outputs[:, i, :],
+                                           valid_task_assignments[:, i])
 
                 loss.backward()
 
-                # 梯度裁剪
                 torch.nn.utils.clip_grad_norm_(
                     self.model.parameters(), max_norm=1.0)
 
@@ -148,21 +142,15 @@ class Train:
                 total_loss += loss.item()
 
             avg_train_loss = total_loss / len(self.dataloader)
-
-            # 验证集评估
             avg_val_loss = self.validate()
-
-            # 调整学习率
             self.scheduler.step(avg_val_loss)
 
-            # Log the average losses to TensorBoard
             self.logger.log_scalar('Loss/train', avg_train_loss, epoch)
             self.logger.log_scalar('Loss/val', avg_val_loss, epoch)
 
             print(
                 f"Epoch {epoch + 1}/{self.num_epochs}, Train Loss: {avg_train_loss}, Val Loss: {avg_val_loss}")
 
-            # 早停逻辑
             best_val_loss, patience_counter, stop = self.early_stopping_check(
                 avg_val_loss, best_val_loss, patience_counter, "best_model.pth")
             if stop:
@@ -175,47 +163,35 @@ class Train:
         ModelManager.load_model(self.model, path, self.device)
 
 
-def init_weights(m):
-    if type(m) == nn.Linear:
-        nn.init.kaiming_uniform_(m.weight, nonlinearity='relu')
-    elif type(m) == nn.TransformerEncoderLayer:
-        nn.init.xavier_uniform_(m.self_attn.in_proj_weight)
-        nn.init.constant_(m.self_attn.in_proj_bias, 0)
-        nn.init.xavier_uniform_(m.linear1.weight)
-        nn.init.constant_(m.linear1.bias, 0)
-        nn.init.xavier_uniform_(m.linear2.weight)
-        nn.init.constant_(m.linear2.bias, 0)
-
-
 if __name__ == "__main__":
     env_config = {
-        "max_entities": 20,
-        "max_tasks": 15,
-        "entity_dim": 6,  # 平台位置 (x, y), 航程, 速度, 探测距离, 可持续时长
-        "task_dim": 4    # 任务优先级, 任务位置 (x, y), 任务类型
+        "max_entities": 10,
+        "max_tasks": 5,
+        "entity_dim": 6,
+        "task_dim": 4
     }
 
     network_config = {
         "entity_num_heads": 2,
         "task_num_heads": 2,
-        "hidden_dim": 256,
+        "hidden_dim": 64,
         "num_layers": 2,
         "mlp_hidden_dim": 128,
-        "output_dim": 5,     # max_tasks
-        "transfer_dim": 64
+        "output_dim": 5,  # 增加一个任务编号
+        "transfer_dim": 128
     }
 
     training_config = {
         "num_samples": 1000,
         "batch_size": 32,
         "lr": 0.001,
-        "num_epochs": 200,
-        "patience": 100,
+        "num_epochs": 50,
+        "patience": 10
     }
 
-    trainer = Train(env_config, network_config, training_config)
+    trainer = Train(env_config, network_config,
+                    training_config, data_file="train_data.h5")
 
-    # 加载现有的模型
     model_path = "best_model.pth"
     try:
         trainer.load_model(model_path)
